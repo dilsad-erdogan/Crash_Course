689889a95df9fccd08a9068349caf7d357f83f63
Dilşad <91341704+dilsad-erdogan@users.noreply.github.com>
2024-04-29 12:53:16 +0300

    auth screens_1_

--- app/(auth)/sign-in.jsx
+++ app/(auth)/sign-in.jsx
@@ -1,11 +1,28 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, ScrollView } from 'react-native'
+import React, { useState } from 'react'
+import { SafeAreaView } from 'react-native-safe-area-context'
+import { images } from '../../constants';
+import FormField from '../../components/FormField';
 
 const SignIn = () => {
-  return (
-    <View>
-      <Text>SignIn</Text>
-    </View>
+  const [form, setForm] = useState({
+    email: '',
+    password: ''
+  });
+
+  return ( 
+    <SafeAreaView className="bg-primary h-full">
+      <ScrollView>
+        <View className="w-full justify-center h-full px-4 my-6">
+          <Image source={images.logo} resizeMode="contain" className="w-[115px] h-[35px]"></Image>
+
+          <Text className="text-2xl text-white text-semibold mt-10 font-semibold">Log in to Aora</Text>
+
+          <FormField title="Email" value={form.email} handleChangeText={(e) => setForm({ ...form, email: e})} otherStyles="mt-7" keyboardType="email-address"></FormField>
+          <FormField title="Password" value={form.password} handleChangeText={(e) => setForm({ ...form, password: e})} otherStyles="mt-7"></FormField>
+        </View>
+      </ScrollView>
+    </SafeAreaView>
   )
 }
 
--- app/index.jsx
+++ app/index.jsx
@@ -1,6 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import { Image, ScrollView, Text, View } from 'react-native';
-import { Link } from 'expo-router';
+import { Redirect, router } from 'expo-router';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { images } from '../constants';
 import CustomButton from '../components/CustomButton';
@@ -9,7 +9,7 @@ export default function App() {
     return (
         <SafeAreaView className="bg-primary h-full">
             <ScrollView contentContainerStyle={{ height: '100%' }}>
-                <View className="w-full justify-center items-center h-full px-4">
+                <View className="w-full justify-center items-center min-h-[85px] px-4">
                     <Image source={images.logo} className="w-[130px] h-[84px]" resizeMode="contain"></Image>
 
                     <Image source={images.cards} className="max-w-[380px] w-full h-[300px]" resizeMode="contain"></Image>
@@ -21,9 +21,11 @@ export default function App() {
 
                     <Text className="text-sm font-pregular text-gray-100 mt-7 text-center">Where creativity meets innovation: embark on a journey of limitless exploration with Aora</Text>
 
-                    <CustomButton title="Continue with Email" handlePress={() => {}} containerStyle="w-full mt-7"></CustomButton>
+                    <CustomButton title="Continue with Email" handlePress={() => router.push('/sign-in')} containerStyle="w-full mt-7"></CustomButton>
                 </View>
             </ScrollView>
+
+            <StatusBar backgroundColor='#161622' style='light'></StatusBar>
         </SafeAreaView>
     );
 }
